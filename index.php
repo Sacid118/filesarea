8541918e3de72e1a379dd0ccab485738cd2cd5a0
Trigga <65285535+Trigga28@users.noreply.github.com>
2024-06-04 14:12:29 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 
+/**
+ * Files and Images Manager
+ *
+ * @version 1.0.0
+ * @author Sacid Sezgin <sezgin@gfg-id.de>
+ * @copyright Sacid Sezgin <sezgin@gfg-id.de>
+ * @link https://github.com/Sacid118/filesarea
+ * @license MIT
+**/
+
 use Kirby\Cms\App as Kirby;
 
-Kirby::plugin('sezgin/filesarea', [
+Kirby::plugin('Trigga28/filesarea', [
 	'areas' => [
 		'files' => [
 			'label'   => 'Dateimanager',
